37c0f876c9867a8eb9a69370793e1faf646f8fc5
Munieru <20086673+munierujp@users.noreply.github.com>
2019-08-13 17:06:55 +0900

    Add API buttons

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -29,18 +29,41 @@
         </button>
       </div>
     </div>
+    <hr>
+    <div>
+      <button
+        :disabled="isLoading"
+        @click="kickPublicApi"
+      >
+        Kick public API
+      </button>
+      <button
+        :disabled="isLoading"
+        @click="kickPrivateApi"
+      >
+        Kick private API
+      </button>
+    </div>
+    <div>
+      <span>Response: {{ response }}</span>
+    </div>
   </div>
 </template>
 
 <script>
 import firebase from '~/modules/firebase'
 
+const { API_BASE_URL } = process.env.config
+const PUBLIC_API_URL = `${API_BASE_URL}public`
+const PRIVATE_API_URL = `${API_BASE_URL}private`
+
 export default {
   asyncData () {
     return {
       isLoading: true,
       isSignedIn: false,
-      user: {}
+      user: {},
+      response: ''
     }
   },
   mounted () {
@@ -62,7 +85,21 @@ export default {
     },
     signOut () {
       firebase.auth().signOut()
+    },
+    async kickPublicApi () {
+      const response = await fetchText(PUBLIC_API_URL)
+      this.response = response
+    },
+    async kickPrivateApi () {
+      const response = await fetchText(PRIVATE_API_URL)
+      this.response = response
     }
   }
+}
+
+async function fetchText (url) {
+  const response = await fetch(url)
+  const text = await response.text()
+  return text
 }
 </script>
